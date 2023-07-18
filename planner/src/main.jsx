d816b3234762491395048c03816b0ce70c85a584
DonovanJJ <donovanchanjiajun2001@gmail.com>
2023-07-18 12:40:19 +0800

    Removed some dependancies that are unused

--- planner/src/main.jsx
+++ planner/src/main.jsx
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="127216979978-it19u2bgpk7p9fmke7ni41aou9h911gr.apps.googleusercontent.com">
     <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </React.StrictMode>
-  </GoogleOAuthProvider>
 );
